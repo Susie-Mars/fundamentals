1f7b1c340635fdc2189d5f8a3f8b5a3ef2df2d96
Susie-Mars <marsnarsbars@gmail.com>
2022-09-26 23:17:57 -0600

    nested_dictionaries

---
+++ nested_dictionaries.py
@@ -0,0 +1,84 @@
+# # UPDATE VALUES IN DICTIONARIES AND LISTS
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+x[1][0]= 15
+print(x)
+students[0]["last_name"]="Bryant"
+print(students)
+sports_directory["soccer"][0]="Andres"
+print(sports_directory)
+z[1][0]=30
+print(z)
+
+# # ITERATE THROUGH A LIST OF DICTIONARIES
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(some_list):
+    for i in some_list:
+        print(i)
+
+iterateDictionary(students)
+
+# GET VALUES THROUGH A LIST OF DICTIONARIES
+
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(len(some_list)):
+        print(some_list[i][key_name])
+
+iterateDictionary2("first_name",students)
+
+iterateDictionary2("last_name", students)
+
+# ITERATE THROUGH A DICTIONARY WITH LIST VALUES
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key, value in dojo.items():
+        print(key, value)
+
+printInfo(dojo)
+
+
+
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+
+
+
